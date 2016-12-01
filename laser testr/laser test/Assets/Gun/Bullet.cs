1a5b49d0a51f2b9e01514e5c3c0dd0eac798e2e3
Spekterops <Spekterops>
2016-12-02 09:21:26 +1000

    First attempt at pushing the laser test

---
+++ laser testr/laser test/Assets/Gun/Bullet.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+    private GameObject hiteffect;
+
+
+    void Start ()
+    {
+       // hiteffect = 
+        Destroy(this.gameObject, 2);
+	}
+	
+	void Update ()
+    {
+	
+	}
+
+    void OnCollisionEnter(Collision other)
+    {
+        
+    }
+}
---
+++ laser testr/laser test/Assets/Gun/GunScript.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class GunScript : MonoBehaviour {
+
+    private float cooldowntime;
+    private float cooldowndifference = 0.1f;
+
+    private float reloadtime;
+    private float reloaddiff = 2;
+    private float ammo ;
+    private float clipsize = 2;
+
+    private bool canfire = true;
+
+
+    private GameObject muzzle;
+    private GameObject butt;
+    public GameObject bullethiteffect;
+
+    public GameObject bullet;
+    private float bulletspeed = 2500;
+
+    void Start ()
+    {
+        ammo = clipsize;
+        muzzle = this.gameObject.transform.GetChild(0).gameObject;
+    }
+	
+
+
+
+
+	void Update ()
+    {
+        firechecks();
+	}
+
+
+
+
+
+    void firechecks()
+    {
+
+        cooldowntime -= Time.deltaTime;
+        reloadtime -= Time.deltaTime;
+
+        if (ammo == 0)
+        {
+            reloadtime = reloaddiff;
+            ammo = clipsize;
+            
+        }
+        if (cooldowntime <= 0 && reloadtime <= 0)
+            canfire = true;
+        else
+            canfire = false;
+ 
+
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (canfire)
+            {
+                muzzle.GetComponent<ParticleSystem>().Play();
+
+                 GameObject _bullet = Instantiate(bullet, muzzle.transform.position, muzzle.transform.rotation) as GameObject;
+                _bullet.GetComponent<Rigidbody>().AddForce(muzzle.transform.forward *  bulletspeed);
+                ammo--;
+                cooldowntime = cooldowndifference;
+            }
+        }
+    }
+
+
+
+
+
+ 
+}
